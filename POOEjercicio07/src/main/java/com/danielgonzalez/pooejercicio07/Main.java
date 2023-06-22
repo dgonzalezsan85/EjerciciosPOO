6834b0732d3f0145c86493eefd9058717004c736
Daniel Gonzalez <dgonzalezsan85@gmail.com>
2023-06-22 15:14:03 -0500

    Ejercicio 7

--- POOEjercicio07/src/main/java/com/danielgonzalez/pooejercicio07/Main.java
+++ POOEjercicio07/src/main/java/com/danielgonzalez/pooejercicio07/Main.java
@@ -1,8 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package com.danielgonzalez.pooejercicio07;
+
+import java.net.SocketTimeoutException;
+
+import com.danielgonzalez.pooejercicio07.Entidades.Persona;
+import com.danielgonzalez.pooejercicio07.Servicios.PersonaServicio;
 
 /**
  *
@@ -14,7 +15,49 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        PersonaServicio ps = new PersonaServicio();
+
+        Persona[] personas = new Persona[4];
+
+        int pesoNormal = 0;
+        int sobrePeso = 0;
+        int bajoPeso = 0;
+        int mayorE = 0;
+        int menorE = 0;
+
+        for (Persona persona : personas) {
+            persona = ps.crearPersona();
+            double imc = ps.calcularIMC(persona);
+            boolean mayorEdad = ps.esMayorDeEdad(persona);
+
+            if (imc == 0) {
+                System.out.println(persona.getNombre() + " tiene un peso normal.");
+                pesoNormal += 1;
+            } else if (imc == -1) {
+                System.out.println(persona.getNombre() + " está debajo del peso");
+                bajoPeso += 1;
+            } else if (imc == 1) {
+                System.out.println(persona.getNombre() + " tiene sobrepeso.");
+                sobrePeso += 1;
+            }
+
+            if (mayorEdad) {
+                System.out.println(persona.getNombre() + " es mayor de edad");
+                mayorE += 1;
+            } else {
+                System.out.println(persona.getNombre() + " no es mayor de edad");
+                menorE += 1;
+            }
+        }
+
+        System.out.println("De las cuatro personas " + (pesoNormal * 100) / 4 + " % tienen peso normal.");
+        System.out.println("De las cuatro personas " + (bajoPeso * 100) / 4 + " % tienen bajo peso.");
+        System.out.println("De las cuatro personas " + (sobrePeso * 100) / 4 + " % tienen sobrepeso.");
+        System.out.println();
+        System.out.println("De las cuatro personas " + (mayorE * 100) / 4 + " % son mayores de edad.");
+        System.out.println("De las cuatro personas " + (menorE * 100) / 4 + " % son menores de edad.");
+
     }
-    
+
 }
--- POOEjercicio07/src/main/java/com/danielgonzalez/pooejercicio07/Servicios/PersonaServicio.java
+++ POOEjercicio07/src/main/java/com/danielgonzalez/pooejercicio07/Servicios/PersonaServicio.java
@@ -4,10 +4,68 @@
  */
 package com.danielgonzalez.pooejercicio07.Servicios;
 
+import java.util.Scanner;
+
+import com.danielgonzalez.pooejercicio07.Entidades.Persona;
+
 /**
  *
  * @author danielgonzalez
  */
 public class PersonaServicio {
-    
+
+    private Scanner leer = new Scanner(System.in).useDelimiter("\n");
+
+    public Persona crearPersona() {
+        Persona persona = new Persona();
+
+        System.out.println("Ingrese el nombre: ");
+        persona.setNombre(leer.next());
+        System.out.println("Ingrese la edad: ");
+        persona.setEdad(leer.nextInt());
+        System.out.println("Ingrese el sexo, 'H' para hombre, 'M' para mujer u 'O' para otro: ");
+        char sexo = Character.toUpperCase(leer.next().charAt(0));
+
+        if (sexo == 'H') {
+            persona.setSexo('h');
+        } else if (sexo == 'M') {
+            persona.setSexo('m');
+        } else if (sexo == 'O') {
+            persona.setSexo('o');
+        } else {
+            throw new IllegalArgumentException("El valor ingresado no es valido.");
+        }
+
+        System.out.println("Ingrese el peso en kg: ");
+        persona.setPeso(leer.nextDouble());
+        System.out.println("Ingrese la altura en mts: ");
+        persona.setAltura(leer.nextDouble());
+
+        return persona;
+    }
+
+    public double calcularIMC(Persona persona) {
+        double resultado = 0;
+        
+        double imc = persona.getPeso() / (Math.pow(persona.getAltura(), 2));
+
+        if (imc < 20) {
+            resultado = -1;
+        } else if (imc >= 20 && imc <= 25) {
+            resultado = 0;
+        } else {
+            resultado = 1;
+        }
+        return resultado;
+    }
+
+    public boolean esMayorDeEdad(Persona persona) {
+
+        if (persona.getEdad() >= 18) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
